5e0c808bd252de2cb4705321c16daa517c535474
lasttimer <shubham2302gupta@gmail.com>
2018-03-28 01:47:50 +0530

    Update forms.py

--- wallet/forms.py
+++ wallet/forms.py
@@ -10,7 +10,7 @@ from wallet.models import balance
 
 
 class LoginForm(forms.Form):
-    phone = forms.CharField(label='',required=True, max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Mobile No.'}))
+    phone = forms.CharField(label='',required=True, min_length=10, max_length=10, widget=forms.TextInput(attrs={'placeholder': 'Mobile No.'}))
     password = forms.CharField(label='', required=True, max_length=30, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
     def clean(self, *args, **kwargs):
@@ -27,7 +27,7 @@ class LoginForm(forms.Form):
  
 class RegistrationForm(forms.Form): 
     username = forms.CharField(label='', required=True, max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    phone = forms.CharField(label='', required=True, max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Mobile No.'}))
+    phone = forms.CharField(label='', required=True, min_length=10, max_length=10, widget=forms.TextInput(attrs={'placeholder': 'Mobile No.'}))
     email = forms.EmailField(label='', required=True, max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     password1 = forms.CharField(required=True, max_length=30, label='', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
     password2 = forms.CharField(required=True, max_length=30, label='', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
@@ -59,7 +59,7 @@ class RegistrationForm(forms.Form):
             User.objects.get(username=username)
         except User.DoesNotExist:
             return username
-        raise forms.ValidationError('Mobile No already registered!')
+        raise forms.ValidationError('The mobile number you entered already exists with another account.')
 
 
     """def clean(self):
@@ -85,29 +85,34 @@ class RegistrationForm(forms.Form):
 
 
 class SendMoneyForm(forms.Form):
-    phone = forms.CharField(label='',required=True,  widget=forms.TextInput(attrs={'placeholder': 'Mobile No.'}))
+    phone = forms.CharField(label='',required=True, min_length=10, max_length=10,  widget=forms.TextInput(attrs={'placeholder': 'Mobile No.'}))
     amount = forms.FloatField(label='', required=True, widget=forms.TextInput(attrs={'placeholder': 'Amount'}))
 
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request' or None)
+        super(SendMoneyForm, self).__init__(*args, **kwargs)
+
     def clean_phone(self):
-        username = self.cleaned_data['phone']
-        if not re.search(r'^\w+$', username):
+        phone = self.cleaned_data['phone']
+        if not re.search(r'^\w+$', phone):
             raise forms.ValidationError('Username can only contain alphanumeric characters and the underscore.')
         try:
-            User.objects.get(username=username)
+            User.objects.get(username=phone)
         except User.DoesNotExist:
             raise forms.ValidationError('MObile number is NOT Registered')
-        return username
+        return phone
 
     def clean_amount(self):
         if 'phone' in self.cleaned_data:
+            phone = self.cleaned_data['phone']
             amount = int(self.cleaned_data['amount'])
-            user = self.cleaned_data['phone']
-            data = balance.objects.get(username=user)
-            var=int(data.balance)
+            usr = self.request.user.username
+            data = balance.objects.get(username=usr)
+            var=data.balance
             print (var)
             print (amount)
             if amount>var:
-                raise  ValidationError("You have Insufficient Balance for this transaction")
+                raise  ValidationError("You have Insufficient Balance for this transaction please check your balance and retry")
             elif amount<=0:
                 raise ValidationError("Please Enter a valid amount")
             return amount
